2b033b0f7d94c6ba2f17d7f525393b1b01aef6b1
lightdance <773617349@qq.com>
2019-04-17 01:16:44 +0800

    添加对外接口方法

--- src/main/java/com/tensor/api/org/service/spark/SparkService.java
+++ src/main/java/com/tensor/api/org/service/spark/SparkService.java
@@ -1,5 +1,9 @@
 package com.tensor.api.org.service.spark;
 
+import com.tensor.api.org.enpity.News;
+
+import java.util.List;
+
 /**
  * 对外可见的接口，提供需要spark完成的服务
  *
@@ -13,4 +17,11 @@ public interface SparkService {
      * @param <T> todo 新闻类么？
      */
     <T> void distinctNews(T news);
+
+    /**
+     * 将参数新闻与已有的全部新闻对比，返回相似度最高的新闻列表
+     * @param news 指定新闻
+     * @return List类型相似新闻列表
+     */
+    List<News> getSimilarity(News news);
 }
--- src/main/java/com/tensor/api/org/service/spark/SparkServiceImpl.java
+++ src/main/java/com/tensor/api/org/service/spark/SparkServiceImpl.java
@@ -1,5 +1,17 @@
 package com.tensor.api.org.service.spark;
 
+import com.tensor.api.org.enpity.News;
+import com.tensor.api.org.service.spark.base.BaseSparkServiceImpl;
+import com.tensor.api.org.service.spark.similarity.SimilarityServiceImpl;
+import com.tensor.api.org.service.spark.statistics.StatisticsService;
+import com.tensor.api.org.service.spark.statistics.StatisticsServiceImpl;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * spark对外接口实现类
  *
@@ -10,5 +22,28 @@ public class SparkServiceImpl implements SparkService{
     @Override
     public <T> void distinctNews(T news) {
         //。。。
+    }
+
+    @Override
+    public List<News> getSimilarity(News news) {
+        BaseSparkServiceImpl.init();
+        StatisticsService statisticsService = new StatisticsServiceImpl();
+
+        JavaPairRDD<String , Integer> wordFrequency1 = statisticsService.getWordFrequency(news);
+
+        System.out.println(wordFrequency1.count());
+
+        SimilarityServiceImpl similarityService = new SimilarityServiceImpl();
+        //此时已经排好序
+        JavaPairRDD<Long , Integer>  similarNewsIdRDD = similarityService.getSimilarityList(wordFrequency1);
+        List<News> newsList = new ArrayList<>();
+        similarNewsIdRDD.foreach(new VoidFunction<Tuple2<Long, Integer>>() {
+            @Override
+            public void call(Tuple2<Long, Integer> longIntegerTuple2) throws Exception {
+                //todo:通过tuple2._1找到对应的新闻记录并加入到List中
+                newsList.add(News.builder().id(longIntegerTuple2._1).build());
+            }
+        });
+        return newsList;
     }
 }

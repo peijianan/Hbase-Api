9bbb3bbc6fd5db5631f947788af43d09b709417b
hdumeteor <39825419+hdumeteor@users.noreply.github.com>
2019-04-17 08:08:20 +0800

    添加get set 方法

--- src/main/java/com/tensor/api/org/enpity/News.java
+++ src/main/java/com/tensor/api/org/enpity/News.java
@@ -16,5 +16,44 @@ public class News {
     private String author;
     private String text;
     private String hashCode;
+        
+    public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNewTitle() {
+		return newTitle;
+	}
+	public void setNewTitle(String newTitle) {
+		this.newTitle = newTitle;
+	}
+	public String getNewType() {
+		return newType;
+	}
+	public void setNewType(String newType) {
+		this.newType = newType;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getHashCode() {
+		return hashCode;
+	}
+	public void setHashCode(String hashCode) {
+		this.hashCode = hashCode;
+	}
+
+    
 
 }

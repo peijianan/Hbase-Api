05a21a3505addc6a2fe27389d31a402785ca7c9b
peijianan <16051425@hdu.edu.cn>
2019-05-28 17:54:47 +0800

    Add files via upload

--- provider/src/main/java/com/tensor/api/org/service/hbase/HBaseNewsService.java
+++ provider/src/main/java/com/tensor/api/org/service/hbase/HBaseNewsService.java
@@ -27,14 +27,14 @@ public interface HBaseNewsService {
      *
      * @return
      */
-    Mono<ResultData<JsonArray>> getAllAuthor();
+    Mono<ResultData<Page>> getAllAuthor();
 
     /**
      * 读取全部标题 返回 行键-标题
      *
      * @return
      */
-    Mono<ResultData<JsonArray>> getAllTitle();
+    Mono<ResultData<Page>> getAllTitle();
 
     /**
      * 根据行键读取新闻 返回对应新闻
@@ -42,7 +42,7 @@ public interface HBaseNewsService {
      * @param rowKey
      * @return
      */
-    Mono<ResultData<JsonArray>> getNewsByRowKey(String rowKey);
+    Mono<ResultData<Page>> getNewsByRowKey(String rowKey);
 
     /**
      * 根据标题读取新闻  返回对应新闻
@@ -50,7 +50,7 @@ public interface HBaseNewsService {
      * @param newTitle
      * @return
      */
-    Mono<ResultData<JsonArray>> getNewsByTitle(String newTitle);
+    Mono<ResultData<Page>> getNewsByTitle(String newTitle);
 
     /**
      * 根据作者读新闻    返回对应新闻
@@ -58,7 +58,7 @@ public interface HBaseNewsService {
      * @param author
      * @return
      */
-    Mono<ResultData<JsonArray>> getNewsByAuthor(String author);
+    Mono<ResultData<Page>> getNewsByAuthor(String author);
 
     /**
      * 根据分类读新闻
@@ -66,5 +66,5 @@ public interface HBaseNewsService {
      * @param newType
      * @return
      */
-    Mono<ResultData<JsonArray>> getNewsByType(String newType);
+    Mono<ResultData<Page>> getNewsByType(String newType);
 }
